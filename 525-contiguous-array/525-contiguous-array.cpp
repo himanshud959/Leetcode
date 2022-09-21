2a69b3f4dee4f0ecb869ef492e63a1de0d17ac1a
Himanshu Dubey <87020183+himanshud959@users.noreply.github.com>
2022-09-21 12:24:47 +0530

    Time: 372 ms (5.02%), Space: 83.8 MB (84.68%) - LeetHub

--- 525-contiguous-array/525-contiguous-array.cpp
+++ 525-contiguous-array/525-contiguous-array.cpp
@@ -4,16 +4,17 @@ public:
         unordered_map<int, int> mp;
         mp[0] = -1;
         int res = 0;
-        int count0 = 0, count1 = 0;
+        int curr = 0;
         for(int i = 0;i < nums.size();i++) {
             int num = nums[i];
-            count0 += num == 0;
-            count1 += num == 1;
-            int key = count0 - count1;
-            if(mp.find(key) != mp.end()) {
-                res = max(res, i - mp[key]);
+            if(num == 0) {
+                num = -1;
+            }
+            curr += num;
+            if(mp.find(curr) != mp.end()) {
+                res = max(res, i - mp[curr]);
             } else {
-                mp[key] = i;
+                mp[curr] = i;
             }
         }
         return res;
